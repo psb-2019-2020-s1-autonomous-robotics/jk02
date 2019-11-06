68f3dce9d1b721f0ed8455cfc63c7807b7bb4d54
konogw373 <57004568+konogw373@users.noreply.github.com>
2019-11-06 09:57:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import ]]jt nytrsu;op['
+Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+brick.sound.beep()
+  
